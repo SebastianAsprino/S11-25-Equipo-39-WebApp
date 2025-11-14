a4837189c06ffcc8b69c0b4c77c3547ce24290ef
Sebastian Asprino <sebastian@asprino.dev>
2025-11-13 21:42:08 -0500

    feat: Configura OpenAPI y CORS en la API
    Inicializa OpenAPI para la documentación de la API y CORS para permitir solicitudes de diferentes dominios.

---
+++ BackEnd/src/index.ts
@@ -0,0 +1,23 @@
+import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors';
+import { openapi } from '@elysiajs/openapi';
+import { Routes } from "./routes";
+
+const app = new Elysia()
+	.use(cors())
+	.use(openapi({
+		documentation: {
+			info: {
+				title: "PET HEALTH TRACKER API",
+				description: "Api pet health tracker.",
+				version: "0.0.1"
+			},
+			tags: [{ name: 'User', description: 'User endpoints' }]
+		}
+	}))
+	.use(Routes)
+	.listen(8000);
+
+console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
